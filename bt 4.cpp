9bc776c80725286151783f7fead5210ef18b7290
tman689 <tranminhan05092000@gmail.com>
2025-01-07 17:00:13 +0700

    Add files via upload

--- bt 4.cpp
+++ bt 4.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-void printArray(int *arr, int size){
-    for (int i = 0; i < size; i++){
+void printArray(int *arr, int *size){
+    for (int i = 0; i < *size; i++){
         printf("Phan tu thu %d: %d\n", i, *(arr + i));
     }
 }
@@ -10,7 +10,7 @@ int main(){
     int arr[] = {1, 2, 3, 4, 5};
     int size = sizeof(arr) / sizeof(arr[0]);
     printf("Cac phan tu trong mang:\n");
-    printArray(arr, size);
+    printArray(arr, &size);
 
     return 0;
 }
